b7b2379385753f5093d9740cb84d34ca6ffd74af
laransom <laransom@gmail.com>
2014-03-28 15:06:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PuppyBook::Application.config.secret_key_base = 'd7e8400c5902cc54cd2fcf1bf0fbb913d6206db6a40e7e60e3affdf4ccb3d327daa3f47bf74d833c6aa69bdbe1f59ddf631908574631a28e0ba8a0c1bb943433'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PuppyBook::Application.config.session_store :cookie_store, key: '_puppy_book_session'
