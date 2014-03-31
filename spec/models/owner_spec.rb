3d3b589056491504dfe04fdf743b2bc8208dd953
laransom <laransom@gmail.com>
2014-03-31 08:41:34 -0400

    added instance method test

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe Owner do
+
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name}
   it { should validate_presence_of :email}
@@ -10,4 +11,12 @@ describe Owner do
     owner = Owner.new(first_name: 'Sterling', last_name: 'Archer', email: 'invalidemail', dog: 'pup')
     expect(owner).to_not be_valid
   end
+
+  context 'full_name method prints full name' do
+    it 'prints full name' do
+      owner = Owner.new(first_name: 'Sterling', last_name: 'Archer', email: 'invalidemail', dog: 'pup')
+      expect(owner.full_name).to eq('Sterling Archer')
+    end
+  end
 end
+
